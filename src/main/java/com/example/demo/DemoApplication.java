456a9f3ea06e1024e6dbd308f6870d38f64476ed
kamalaaouad <kamalaaouad1999@gmail.com>
2021-01-05 00:50:06 +0100

    crud Product with springboot ça marche

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.demo")
 public class DemoApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DemoApplication.class, args);
+        System.out.println("hello");
     }
 
 }
